56af3bb4a0146a2d4b6217da4f65e820e401fefb
Shivam <shivam09084@gmail.com>
2024-12-03 10:21:53 +0530

    inital commit

---
+++ src/main/java/com/coder/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.coder.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	@PreAuthorize("hasAuthority('ROLE_USER')")
+	public String home() {
+		
+		return"home";
+	}
+
+	@RequestMapping("/index")
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	public String index() {
+		
+		return"index";
+	}
+	
+	@RequestMapping("/about")
+	@PreAuthorize("hasAuthority('ROLE_USER')")
+   public String about() {
+		
+		return"about";
+	}
+}
